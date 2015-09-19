dd17f3a53f392b92de6407a64a9b5a30943cc15f
Shohei Nakayama <sho1218ruri@gmail.com>
2015-09-19 17:46:05 +0900

    Logger Classの実装

--- app/src/main/java/com/shohei/put_on/controller/utils/DebugUtil.java
+++
@@ -1,9 +0,0 @@
-package com.shohei.put_on.controller.utils;
-
-/**
- * Created by nakayamashohei on 15/08/10.
- */
-public class DebugUtil {
-    //Debug常態かどうかのフラグ
-    public final static boolean DEBUG = true;
-}
---
+++ app/src/main/java/com/shohei/put_on/controller/utils/Logger.java
@@ -0,0 +1,7 @@
+package com.shohei.put_on.controller.utils;
+
+/**
+ * Created by nakayamashohei on 15/09/19.
+ */
+public class Logger {
+}
