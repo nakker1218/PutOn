198aec3718f215f83bb289918f45402d5d4203be
Shohei Nakayama <nakker1218@gmail.com>
2017-03-01 19:20:24 +0900

    [WIP]]

---
+++ app/src/main/java/me/nakker/put_on/view/activity/MainActivity2.java
@@ -0,0 +1,13 @@
+package me.nakker.put_on.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
